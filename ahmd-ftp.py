1df29e7587acf6ad6b03cc8dadbe66aebbdb5f18
navinraj <navin.raj@datakaveri.org>
2023-03-23 15:08:54 +0530

    changes made

--- ahmd-ftp.py
+++ ahmd-ftp.py
@@ -1,17 +1,22 @@
 import os
+import subprocess
 from subprocess import call,Popen,PIPE
 
+# cmd="dir"
+# process = Popen(cmd, shell=True)
+# process.communicate ()
+subprocess.call('cd ahmd_files', shell=True)
 
-cmd = "git status"
+# cmd = "git status"
+# process = Popen(cmd, shell=True)
+# process.communicate ()
 
-process = Popen(cmd, shell=True)
-process.communicate ()
+# cmd = "git add . "
+# process = Popen(cmd, shell=True)
+# process.communicate ()
 
-cmd = "git add . "
+# cmd = 'git commit -m "newfiles added"'
+# process = Popen(cmd, shell=True)
+# process.communicate ()
 
-process = Popen(cmd, shell=True)
-process.communicate ()
-cmd = "git commit -m 'newfiles added' "
 
-process = Popen(cmd, shell=True)
-process.communicate ()
